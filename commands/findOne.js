ee31e31fdcd9d53770eda23941590bcf4fd2dd4f
sugawara ryousuke <rsksound@gmail.com>
2014-12-25 18:24:19 +0900

    commit

--- commands/findOne.js
+++ commands/findOne.js
@@ -1,20 +1,27 @@
+/*
+*dataStoreの中身を抽出
+*/
+
 var MilkCocoa = require('../lib/milkcocoa');
 
 module.exports = function (info){
 	var milkcocoa = new MilkCocoa('https://'+ info.appId +'.mlkcca.com');
 	var ds = milkcocoa.dataStore(info.dataStore);
-
-	if(!process.argv[3] || !process.argv[4] || !process.argv[5]){
+	
+	if(!process.argv[3] || !process.argv[4]){
 		console.log('empty argv');
 		return;
 	}
 
-	var id = process.argv[3];
-	var key = process.argv[4];
-	var value = process.argv[5];
-
+	var key = process.argv[3];
+	var value = process.argv[4];
 	var obj = {};
 	obj[key] = value;
-	ds.set(id, obj);
-	console.log("set! done");
-};
+	
+	var query = ds.query(obj);
+	
+	//done();
+	query.done(function(data) {
+		console.log(data);
+	});
+}
